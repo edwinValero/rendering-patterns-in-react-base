dcc3c371b5657eb74c7b2b2cd7e698f6f340c8ce
edwin valero <edwin.valero@teaminternational.com>
2025-06-11 15:14:02 -0400

    feat: create render props task

--- src/components/RenderProps1/RenderProps1.tsx
+++ src/components/RenderProps1/RenderProps1.tsx
@@ -1,6 +1,14 @@
+import React from 'react';
+
+type ChildComponentProps = {
+  render: (name: string) => React.JSX.Element;
+};
+
+export const ChildComponent: React.FC<ChildComponentProps> = ({ render }) => {
+  const name = 'Edw Val';
+  return <div>{render(name)}</div>;
+};
+
 export const ParentComponent = () => {
-  return (
-    <>
-    </>
-  );
+  return <ChildComponent render={(name) => <p>Hello {name}!</p>} />;
 };
--- src/components/RenderProps2/ParentComponent.tsx
+++ src/components/RenderProps2/ParentComponent.tsx
@@ -1,4 +1,12 @@
-const ParentComponent = () => {
+import { useState } from 'react';
+
+type ParentComponentProps = {
+  render: (data: string[]) => React.ReactNode;
+};
+
+const ParentComponent: React.FC<ParentComponentProps> = ({ render }) => {
+  const [list] = useState<string[]>(['Banana 🍌', 'Apple 🍎', 'Orange 🍊']);
+  return <ul>{render(list)}</ul>;
 };
 
 export default ParentComponent;
