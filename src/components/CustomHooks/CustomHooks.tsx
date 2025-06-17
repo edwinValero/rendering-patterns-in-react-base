a83c52dd8e5d4649f0e6e8fb903bc5500bf6d4cc
edwin valero <edwin.valero@teaminternational.com>
2025-06-17 14:12:38 -0400

    high order component and custom hooks

--- src/components/CustomHooks/CustomHooks.tsx
+++ src/components/CustomHooks/CustomHooks.tsx
@@ -1,10 +1,26 @@
-const UserComponent = () => {
+import { useState } from 'react';
+
+function useLoading(initialLoagind: boolean) {
+  const [isLoading, setLoading] = useState(initialLoagind);
+  return { isLoading, setLoading };
+}
+
+type UserProps = {
+  name: string;
+};
+
+const UserComponent: React.FC<UserProps> = ({ name }) => {
+  const { isLoading, setLoading } = useLoading(false);
+
+  if (isLoading) return <div>Loading ...</div>;
   return (
     <div>
+      <p>{`Hello ${name} !!`}</p>
+      <button onClick={() => setLoading(true)}>Simulate loading</button>
     </div>
   );
 };
 
 export const ParentComponent = () => {
-  return <UserComponent />;
+  return <UserComponent name='Edw Val test' />;
 };
